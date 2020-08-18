25b7c6ff29617902077b35c9afb12926512c2594
hiagopsilva <hiagohps360@gmail.com>
2020-08-17 23:53:12 -0300

    refactor: message response

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,7 +1,11 @@
-import { User } from '@models/User'
+// import { User } from '@models/User'
 
-export class UsersController {
-  teste () {
-    const user = new User()
+class UsersController {
+  index (request, response) {
+    // const user = new User()
+
+    return response.json({ message: 'Welcome to Template Backend.   by: Hiago Prates @hiagopsilva' })
   }
 }
+
+export default new UsersController()
