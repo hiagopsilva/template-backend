2d116f2f2e8221775dd711e3b249c01f89014f19
hiagopsilva <hiagohps360@gmail.com>
2020-08-18 00:07:21 -0300

    feat(database config): database config

---
+++ src/database/connection.ts
@@ -0,0 +1,9 @@
+// import mongoose from 'mongoose'
+
+function connectionDatabase () {
+  const reponse = process.env.MESSAGE_DATABASE
+
+  return console.log(reponse)
+}
+
+export default connectionDatabase
